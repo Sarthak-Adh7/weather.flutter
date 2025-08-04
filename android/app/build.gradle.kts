6307e2c3d61033100cc41836753f0a81e525ab39
Sarthak-Adh7 <adhikarysarthak3@gmail.com>
2025-08-04 23:22:04 +0545

    add

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,15 +1,14 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id 'com.google.gms.google-services'
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.weather"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 34
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,20 +20,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.weather"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 23  // ✅ Updated from 21 to 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -45,6 +39,6 @@ flutter {
 }
 
 dependencies {
-    implementation platform('com.google.firebase:firebase-bom:34.0.0')
+    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+    // Add other Firebase dependencies as needed
 }
-
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,12 +1,22 @@
+// android/build.gradle.kts
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.2")  // Use your current gradle plugin version
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")  // Kotlin plugin version
+        classpath("com.google.gms:google-services:4.4.3")  // Google services plugin
+    }
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
-}
-dependencies{
-    classpath 'com.google.gms.google-services:'4.4.3'
-
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
@@ -16,6 +26,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
